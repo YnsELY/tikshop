dba00813d9e819b71c5f5bf768fa32315c1c7060
Yns <yanis@el-yassiri.com>
2025-08-22 19:45:23 +0200

    Updated ProductDetail.tsx

--- src/components/product/ProductDetail.tsx
+++ src/components/product/ProductDetail.tsx
@@ -721,8 +721,7 @@ export const ProductDetail: React.FC<ProductDetailProps> = ({ productId: propPro
            <div className="flex flex-col sm:flex-row space-y-3 sm:space-y-0 sm:space-x-4">
               <Button
                 onClick={handleAddToCart}
-                variant="outline"
-                size="lg"
+               className="bg-[#8b6b5a] hover:bg-[#755441] text-white"
                 disabled={!selectedVariant || selectedVariant.stock === 0}
               >
                 <ShoppingCart className="w-5 h-5 mr-2" />
@@ -731,7 +730,7 @@ export const ProductDetail: React.FC<ProductDetailProps> = ({ productId: propPro
               
               <Button
                 onClick={() => navigate('/')}
-               className="flex-1 bg-[#8b6b5a] hover:bg-[#755441] text-white"
+               className="bg-gray-100 hover:bg-gray-200 text-gray-700 border border-gray-300"
                 size="lg"
                 className="flex-1"
               >
